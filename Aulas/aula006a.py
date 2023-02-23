322c91bdf49d7c367c2a3963e187df827a8a4b28
João Menicuci <joao_menicuci@hotmail.com>
2023-02-23 14:18:34 -0300

    Resolução de Exercícios

---
+++ Aulas/aula006a.py
@@ -0,0 +1,7 @@
+# Tipos Primitivos em Python
+# int(10), float(10.5), bool(True / False), str('olá')
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1 + n2
+print('A soma entre {} e {} vale: {}'.format(n1, n2, s))
---
+++ Aulas/aula006b.py
@@ -0,0 +1,6 @@
+# n = float(input('Digite um valor: '))
+n = (input('Digite algo: '))
+
+# print(n.isnumeric())
+# print(n.isalpha())
+print(n.isalnum())
