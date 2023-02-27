87084da4d97cd8b7391b0eb5fc713eeab5b94172
João Menicuci <joao_menicuci@hotmail.com>
2023-02-27 16:33:08 -0300

    Resulição de Exercícios

---
+++ Exercícios/ex054.py
@@ -0,0 +1,15 @@
+# Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+
+for pess in range(1, 8):
+    nasc = int(input('Em que ano a pessoa nasceu? '))
+    idade = atual - nasc
+    if idade >= 21:
+        totmaior += 1
+    else:
+        totmenor += 1
+print('Ao todo tivemos {} maior de idade, e {} pessoas menor de idade.'.format(totmaior, totmenor))
