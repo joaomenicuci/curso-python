66291a21fbec96c8df6b7084a4b4a5e4a41614ba
João Menicuci <joao_menicuci@hotmail.com>
2023-02-28 13:45:59 -0300

    Update aula015.py

--- Aulas/aula015.py
+++ Aulas/aula015.py
@@ -1,10 +1,21 @@
 #   Interrompendo Repetições
     # Tipo de Condição:     while:
 
-# Exercício 1
+#   Exercício 1
 
 cont = 1
 while True:
     print(cont, ' ', end='')
     cont += 1
 print('Fim')
+
+#   Exercício 2
+
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+# print('A soma vale {}.'.format(s))
+print(f'A soma vale {s}')
