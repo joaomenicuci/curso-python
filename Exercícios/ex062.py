ebca89a835fa62ab2a2422828d066d580be2d404
João Menicuci <joao_menicuci@hotmail.com>
2023-02-28 12:04:36 -0300

    Resolução de Exercícios

---
+++ Exercícios/ex062.py
@@ -0,0 +1,22 @@
+# Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos. 
+
+print('-='*10)
+print('   GERADOR DE PA')
+print('-='*10)
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+print('-='*10)
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} →'.format(termo), end=' ')
+        termo += razao
+        cont += 1
+    print('Pausa')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados'.format(total))
