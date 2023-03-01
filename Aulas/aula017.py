1311af3bc8b9d2bcc548f5773619b5d98858b33a
João Menicuci <joao_menicuci@hotmail.com>
2023-03-01 11:13:42 -0300

    Atualização Aula 17

---
+++ Aulas/aula017.py
@@ -0,0 +1,6 @@
+#   Variáveis Compostas - Listas
+    # As listas são variáveis compostas que permitem armazenar vários valores em uma mesma estrutura, acessíveis por chaves individuais.
+    # Listas são definidas entre colchetes [].
+
+#   Exercício 1
+
